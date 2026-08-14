31a2d5e498cb6884ef9c390766b12032d4a64e2c
Stefan Ziegler <2767800+edigonzales@users.noreply.github.com>
2026-08-14 10:53:13 +0000

    Render SQL/MM curves without linearizing preview text

--- hop-geometry-type/src/main/java/com/atolcd/hop/core/row/value/ValueMetaGeometry.java
+++ hop-geometry-type/src/main/java/com/atolcd/hop/core/row/value/ValueMetaGeometry.java
@@ -121,7 +121,9 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
 
             Geometry geometry = getGeometry(object);
 
-            if (GeometryUtils.getCoordinateDimension(geometry) == 3) {
+            if (CurveGeometrySupport.isCurveGeometry(geometry)) {
+              string = CurveGeometrySupport.writeWkt(geometry);
+            } else if (GeometryUtils.getCoordinateDimension(geometry) == 3) {
               string = new WKTWriter(3).write(geometry);
             } else {
               string = new WKTWriter(2).write(geometry);
@@ -511,18 +513,6 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
     }
   }
 
-  /**
-   * Return ValueMetaGeometry if sql column type is spatial column
-   *
-   * @param databaseMeta
-   * @param name
-   * @param rm
-   * @param index
-   * @param ignoreLength
-   * @param lazyConversion
-   * @return
-   * @throws HopDatabaseException
-   */
   @Override
   public IValueMeta getValueFromSqlType(
       IVariables variables,
@@ -540,45 +530,30 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
       String columnTypeName = rm.getColumnTypeName(index);
       boolean isDatabaseGeometryColumn;
 
-      // Postgis
       if (databaseMeta.getIDatabase().isPostgresVariant()
           && type == java.sql.Types.OTHER
           && columnTypeName.equalsIgnoreCase("GEOMETRY")) {
-
         isDatabaseGeometryColumn = true;
-
-        // Oracle Spatial/Locator
       } else if (databaseMeta.getIDatabase().isOracleVariant()
           && type == java.sql.Types.STRUCT
           && columnTypeName.equalsIgnoreCase("MDSYS.SDO_GEOMETRY")) {
-
         isDatabaseGeometryColumn = true;
-
-        // MySQL
       } else if (databaseMeta.getIDatabase().isMySqlVariant()
           && type == java.sql.Types.BINARY
           && columnTypeName.equalsIgnoreCase("GEOMETRY")) {
-
         isDatabaseGeometryColumn = true;
-        // MSSQL
       } else if (databaseMeta.getIDatabase().getClass().getSimpleName().contains("MsSqlServer")
           && type == java.sql.Types.VARBINARY
           && columnTypeName.equalsIgnoreCase("GEOMETRY")) {
-
         isDatabaseGeometryColumn = true;
-
       } else {
-
         isDatabaseGeometryColumn = false;
       }
 
-      // Return ValueMetaGeometry
       if (isDatabaseGeometryColumn) {
-
         ValueMetaGeometry valueMeta = new ValueMetaGeometry(name);
         getOriginalColumnMetadata(valueMeta, rm, index, ignoreLength);
         return valueMeta;
-
       } else {
         return null;
       }
@@ -588,69 +563,42 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
     }
   }
 
-  /**
-   * Get Jts Geometry from resultset
-   *
-   * @param databaseInterface
-   * @param resultSet
-   * @param index
-   * @return
-   * @throws HopDatabaseException
-   */
   @SuppressWarnings("unchecked")
   @Override
   public Object getValueFromResultSet(IDatabase databaseInterface, ResultSet resultSet, int index)
       throws HopDatabaseException {
 
     try {
-
       Geometry geometry = null;
       Integer srid = null;
 
-      // Postgis
       if (databaseInterface.isPostgresVariant()) {
-
         String wkt = resultSet.getString(index + 1);
         if (wkt != null) {
-          net.postgis.jdbc.geometry.Geometry pgGeometry = GeometryBuilder.geomFromString(resultSet.getString(index + 1));
+          net.postgis.jdbc.geometry.Geometry pgGeometry =
+              GeometryBuilder.geomFromString(resultSet.getString(index + 1));
           String type = pgGeometry.getTypeString().trim();
           String coords = pgGeometry.getValue().trim();
           srid = pgGeometry.getSrid();
           geometry = new WKTReader().read(type + coords);
         }
-
-        // Oracle Spatial/Locator
       } else if (databaseInterface.isOracleVariant()) {
-
         throw new HopDatabaseException(
             toStringMeta()
                 + " : Oracle Spatial support is not available in this standalone build; use WKT extraction in SQL.");
-
-        // MySQL
       } else if (databaseInterface.isMySqlVariant()) {
-
         int byteOrder = ByteOrderValues.LITTLE_ENDIAN;
-
         byte[] bytes = resultSet.getBytes(index + 1);
         if (bytes != null) {
-
           ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-
-          // SRID
           byte[] sridBytes = new byte[4];
           inputStream.read(sridBytes);
           srid = ByteOrderValues.getInt(sridBytes, byteOrder);
-
-          // Geometry
           geometry = new WKBReader().read(new InputStreamInStream(inputStream));
         }
-
-        // MSSQL
       } else if (databaseInterface.getClass().getSimpleName().contains("MsSqlServer")) {
-
         byte[] bytes = resultSet.getBytes(index + 1);
         if (bytes != null) {
-
           org.locationtech.jts.geom.Geometry ms_geometry =
               JTS.to(org.geolatte.geom.codec.db.sqlserver.Decoders.decode(bytes));
           srid = ms_geometry.getSRID();
@@ -663,7 +611,6 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
       if (srid != null) {
         geometry.setSRID(srid);
       }
-
       return geometry;
 
     } catch (Exception e) {
@@ -677,17 +624,6 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
     }
   }
 
-  /**
-   * Get Sql type name for geometry data type
-   *
-   * @param databaseInterface
-   * @param tk
-   * @param pk
-   * @param use_autoinc
-   * @param add_fieldname
-   * @param add_cr
-   * @return
-   */
   @Override
   public String getDatabaseColumnTypeDefinition(
       IDatabase databaseInterface,
@@ -699,30 +635,21 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
 
     String retval = null;
 
-    // Postgis or Oracle Spatial/Locator or Mysql
     if (databaseInterface.isPostgresVariant()
         || databaseInterface.isOracleVariant()
         || databaseInterface.isMySqlVariant()) {
-
       if (add_fieldname) {
         retval = getName() + " ";
       } else {
         retval = "";
       }
 
-      // Postgis
       if (databaseInterface.isPostgresVariant()) {
         retval += "GEOMETRY";
-
-        // Oracle Spatial/Locator
       } else if (databaseInterface.isOracleVariant()) {
         retval += "SDO_GEOMETRY";
-
-        // Mysql
       } else if (databaseInterface.isMySqlVariant()) {
         retval += "GEOMETRY";
-
-        // MSSQL
       } else if (databaseInterface.getClass().getSimpleName().contains("MsSqlServer")) {
         retval += "GEOMETRY";
       }
@@ -731,112 +658,69 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
         retval += Const.CR;
       }
     }
-
     return retval;
   }
 
-  /**
-   * Encode geometry to for spatial column
-   *
-   * @param databaseMeta
-   * @param preparedStatement
-   * @param index
-   * @param data
-   * @throws HopDatabaseException
-   */
   @Override
   public void setPreparedStatementValue(
       DatabaseMeta databaseMeta, PreparedStatement preparedStatement, int index, Object data)
       throws HopDatabaseException {
 
     try {
-
-      // Postgis
       if (databaseMeta.getIDatabase().isPostgresVariant()) {
-
         Geometry geometry = getGeometry(data);
-
         if (geometry != null) {
-
-          String wkt = null;
-
+          String wkt;
           if (GeometryUtils.getCoordinateDimension(geometry) == 3) {
             wkt = new WKTWriter(3).write(geometry);
           } else {
             wkt = new WKTWriter(2).write(geometry);
           }
-
           if (geometry.getSRID() > 0) {
             wkt = "SRID=" + geometry.getSRID() + ";" + wkt;
           }
-
           PGgeometryLW pgGeom = new PGgeometryLW(wkt);
           preparedStatement.setObject(index, pgGeom, Types.OTHER);
-
         } else {
-
           preparedStatement.setObject(index, null, Types.OTHER);
         }
-
-        // Oracle Spatial/Locator
       } else if (databaseMeta.getIDatabase().isOracleVariant()) {
-
         throw new HopDatabaseException(
             toStringMeta()
                 + " : Oracle Spatial support is not available in this standalone build; use SQL conversion to WKT/WKB.");
-
-        // Mysql
       } else if (databaseMeta.getIDatabase().isMySqlVariant()) {
-
         Geometry geometry = getGeometry(data);
-
         if (geometry != null) {
-
           ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
           byte[] sridBytes = new byte[4];
           ByteOrderValues.putInt(geometry.getSRID(), sridBytes, ByteOrderValues.BIG_ENDIAN);
           outputStream.write(sridBytes);
-
           WKBWriter writer = new WKBWriter(2, ByteOrderValues.BIG_ENDIAN);
           writer.write(geometry, new OutputStreamOutStream(outputStream));
-
           preparedStatement.setBytes(index, outputStream.toByteArray());
-
         } else {
-
           preparedStatement.setObject(index, null, Types.BINARY);
         }
-
-        // MSSQL
       } else if (databaseMeta.getIDatabase().getClass().getSimpleName().contains("MsSqlServer")) {
-
         Geometry geometry = getGeometry(data);
-
         if (geometry != null) {
-
-          String wkt = null;
-
+          String wkt;
           if (GeometryUtils.getCoordinateDimension(geometry) == 3) {
             wkt = new WKTWriter(3).write(geometry);
           } else {
             wkt = new WKTWriter(2).write(geometry);
           }
-
           if (geometry.getSRID() > 0) {
             wkt = "SRID=" + geometry.getSRID() + ";" + wkt;
           }
-
           org.locationtech.jts.io.WKTReader reader = new org.locationtech.jts.io.WKTReader();
           org.locationtech.jts.geom.Geometry msGeom = reader.read(wkt);
           preparedStatement.setBytes(
               index, org.geolatte.geom.codec.db.sqlserver.Encoders.encode(JTS.from(msGeom)));
-
         } else {
-
           preparedStatement.setObject(index, null, Types.VARBINARY);
         }
       }
-
     } catch (Exception e) {
       throw new HopDatabaseException(
           toStringMeta() + " : Unable to set Geometry on prepared statement on index " + index, e);
@@ -848,7 +732,6 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
     boolean n1 = isNull(data1);
     boolean n2 = isNull(data2);
 
-    // null is always smaller!
     if (n1 && !n2) {
       return -1;
     }
@@ -864,48 +747,33 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
       case IValueMeta.TYPE_STRING:
         String one = getString(data1);
         String two = getString(data2);
-
-        if (caseInsensitive) {
-          cmp = one.compareToIgnoreCase(two);
-        } else {
-          cmp = one.compareTo(two);
-        }
+        cmp = caseInsensitive ? one.compareToIgnoreCase(two) : one.compareTo(two);
         break;
-
       case IValueMeta.TYPE_INTEGER:
         cmp = getInteger(data1).compareTo(getInteger(data2));
         break;
-
       case IValueMeta.TYPE_NUMBER:
         cmp = Double.compare(getNumber(data1).doubleValue(), getNumber(data2).doubleValue());
         break;
-
       case IValueMeta.TYPE_DATE:
-        cmp =
-            Long.valueOf(getDate(data1).getTime())
-                .compareTo(Long.valueOf(getDate(data2).getTime()));
+        cmp = Long.valueOf(getDate(data1).getTime()).compareTo(Long.valueOf(getDate(data2).getTime()));
         break;
-
       case IValueMeta.TYPE_BIGNUMBER:
         cmp = getBigNumber(data1).compareTo(getBigNumber(data2));
         break;
-
       case IValueMeta.TYPE_BOOLEAN:
         if (getBoolean(data1).booleanValue() == getBoolean(data2).booleanValue()) {
-          cmp = 0; // true == true, false == false
+          cmp = 0;
         } else if (getBoolean(data1).booleanValue() && !getBoolean(data2).booleanValue()) {
-          cmp = 1; // true > false
+          cmp = 1;
         } else {
-          cmp = -1; // false < true
+          cmp = -1;
         }
         break;
-
       case IValueMeta.TYPE_BINARY:
         byte[] b1 = (byte[]) data1;
         byte[] b2 = (byte[]) data2;
-
         int length = b1.length < b2.length ? b1.length : b2.length;
-
         for (int i = 0; i < length; i++) {
           cmp = b1[i] - b2[i];
           if (cmp != 0) {
@@ -913,31 +781,18 @@ public class ValueMetaGeometry extends ValueMetaBase implements GeometryInterfac
             break;
           }
         }
-
         cmp = b1.length - b2.length;
-
         break;
-
       case TYPE_GEOMETRY:
         String geom1 = getString(data1);
         String geom2 = getString(data2);
-
-        if (caseInsensitive) {
-          cmp = geom1.compareToIgnoreCase(geom2);
-        } else {
-          cmp = geom1.compareTo(geom2);
-        }
+        cmp = caseInsensitive ? geom1.compareToIgnoreCase(geom2) : geom1.compareTo(geom2);
         break;
-
       default:
         throw new HopValueException(
             toString() + " : Comparing values can not be done with data type : " + getType());
     }
 
-    if (isSortedDescending()) {
-      return -cmp;
-    } else {
-      return cmp;
-    }
+    return isSortedDescending() ? -cmp : cmp;
   }
 }
